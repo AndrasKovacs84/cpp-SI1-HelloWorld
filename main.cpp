0ae2822c12f3c9e16935784000a84925d43de36c
Apha84 <ka841001@gmail.com>
2017-09-23 16:50:29 +0200

    Takes name to greet from command line argument

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, char *argv[]) {
-    std::cout << "Hello World!" << std::endl;
+
+    string fullname;
+
+    for (int i = 1; i < argc ; ++i) {
+           fullname += argv[i];
+           fullname += " ";
+    }
+
+    if(fullname.length() == 0){
+        cout << "Hello World!" << endl;
+    } else {
+        cout << "Hello " + fullname << endl;
+    }
+
     return 0;
 }
